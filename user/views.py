047a8b93a016cb0e2615ff98cb98f9bbff87e97b
liyingfei <1780810607@qq.com>
2019-10-22 21:26:41 +0800

    index-紧凑

--- user/views.py
+++ user/views.py
@@ -9,7 +9,11 @@ from random import randint
 from FM.settings import EMAIL_FROM
 from django.contrib.auth.decorators import login_required
 import base64
+<<<<<<< HEAD
 import face_recognition
+=======
+#import face_recognition
+>>>>>>> index-紧凑
 import os
 
 
